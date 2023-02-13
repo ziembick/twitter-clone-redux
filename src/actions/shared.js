c3be2d893f8c1f4dbee925fec141650bd362723e
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-13 16:49:01 -0300

    setAuthedUser

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,7 @@
 import { getInitialData } from "../utils/api";
 import { receiveTweets } from "./tweets";
 import { receiveUsers } from "./users";
+import { setAuthedUser } from "./authedUser";
 
 const AUTHED_ID = "tylermcginnis";
 
@@ -9,7 +10,7 @@ export function handleInitialData() {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
-      dispatch();
+      dispatch(setAuthedUser(AUTHED_ID));
     });
   };
 }
