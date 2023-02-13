f0ad0689fe4f2bcfb19751267d056b89c47a2647
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-13 16:47:53 -0300

    set authed user function

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,11 +2,14 @@ import { getInitialData } from "../utils/api";
 import { receiveTweets } from "./tweets";
 import { receiveUsers } from "./users";
 
+const AUTHED_ID = "tylermcginnis";
+
 export function handleInitialData() {
   return (dispatch) => {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
+      dispatch();
     });
   };
 }
