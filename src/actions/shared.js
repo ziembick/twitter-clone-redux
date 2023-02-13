1882ab96da40ed3208822c5830ef02c5d4607983
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-13 16:43:38 -0300

    handle initial data added dispatch to users and tweets

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,12 @@
 import { getInitialData } from "../utils/api";
+import { receiveTweets } from "./tweets";
+import { receiveUsers } from "./users";
 
 export function handleInitialData() {
   return (dispatch) => {
     return getInitialData().then(({ users, tweets }) => {
-        
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
     });
   };
 }
