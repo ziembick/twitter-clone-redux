0e04c6339e382a59fc77b1c367e35a2d13da6f8f
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-14 14:28:28 -0300

    parent Tweet function

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,17 +2,22 @@ import { connect } from "react-redux";
 import { formatTweet } from "../utils/helpers";
 
 const Tweet = (props) => {
-    console.log(props)
+  if (props.tweet === null) {
+    return <p>This Tweet doesn't exist</p>;
+  }
 
   return <div className="tweet"></div>;
 };
 
 const mapStateToProps = ({ authedUser, users, tweets }, { id }) => {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser),
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null,
   };
 };
 
