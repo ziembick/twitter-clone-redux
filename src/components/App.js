37def24d3ab4ca338ec3215bb1dd79579a8260eb
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-14 15:21:44 -0300

    loading bar

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,22 @@ import { useEffect } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
+import { LoadingBar } from "react-redux-loading-bar";
 
 const App = (props) => {
   useEffect(() => {
     props.dispatch(handleInitialData());
   }, []);
-  return <div>{props.loading === true ? null : <Dashboard />}</div>;
+  return (
+    <div>
+      <LoadingBar />
+      {props.loading === true ? null : <Dashboard />}
+    </div>
+  );
 };
 
 const mapStateToProps = ({ authedUser }) => ({
   loading: authedUser === null,
 });
 
-export default connect()(App);
+export default connect(mapStateToProps)(App);
