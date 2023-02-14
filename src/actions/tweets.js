3f92ffee1b9da32f0082be01f399cabf307905c3
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-14 15:38:09 -0300

    like tweet reducer

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,7 @@
 import { saveLikeToggle } from "../utils/api";
 
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
-export const TOGGLE_TWEET = "TOGGLE_TWEET"
+export const TOGGLE_TWEET = "TOGGLE_TWEET";
 
 export function receiveTweets(tweets) {
   return {
@@ -10,24 +10,23 @@ export function receiveTweets(tweets) {
   };
 }
 
-
-function toggleTweet ({id, authedUser, hasLiked}) {
+function toggleTweet({ id, authedUser, hasLiked }) {
   return {
     type: TOGGLE_TWEET,
     id,
     authedUser,
     hasLiked,
-  }
+  };
 }
 
-export function handleToggleTweet (info) {
+export function handleToggleTweet(info) {
   return (dispatch) => {
-    dispatch(toggleTweet(info))
+    dispatch(toggleTweet(info));
 
     return saveLikeToggle(info).catch((e) => {
-      console.warn("Error in handleToggleTweet: ", e)
-      dispatch(toggleTweet(info))
-      alert("There was an error liking the tweet. Try again.")
-    })
-  }
+      console.warn("Error in handleToggleTweet: ", e);
+      dispatch(toggleTweet(info));
+      alert("There was an error liking the tweet. Try again.");
+    });
+  };
 }
